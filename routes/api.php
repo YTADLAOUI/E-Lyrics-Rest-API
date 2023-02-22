2f5ebdc7ae7183d4e00f842a65b1b112813678d3
anasmoundir <anasmoundir1996@gmail.com>
2023-02-22 14:55:25 +0100

    a

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\AlbumController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ControllerLyrics;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,20 +28,5 @@ Route::apiResource('/artist', ArtistController::class);
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
-    Route::post('logout', 'logout');
-
+   
 });
-Route::resource('/lyrics',ControllerLyrics::class);
-
-Route::group(['middleware' => ['token-verify']], function() {
-
-    Route::post('logout',[AuthController::class,'logout']);
-    Route::post('profil',[AuthController::class,'profil']);
-
-});
-
-//fouad
-Route::apiResource('albums', AlbumController::class);
-//fouad
-
-
